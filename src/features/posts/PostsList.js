d6e972b4178b9d711d4ac99415da633f2ac94a3e
Duje Vatavuk <duje@vatavuk.com>
2024-02-14 02:09:17 +0100

    extract PostExcerpt into its own jsx element

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -5,8 +5,27 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { PostAuthor } from "./PostAuthor";
 import { TimeAgo } from "./TimeAgo";
 import { ReactionButtons } from "./ReactionButtons";
+import { Spinner } from "../../components/Spinner";
 
 import { selectAllPosts, fetchPosts } from "./postsSlice";
+
+const PostExcerpt = ({ post }) => { 
+  return (
+    <article className="post-excerpt">
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  );
+};
 
 export const PostsList = () => {
   const dispatch = useDispatch();
@@ -22,18 +41,7 @@ export const PostsList = () => {
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));//needs to be immutable, so we use slice() to create a copy of the array before sorting it.
 
   const renderedPosts = orderedPosts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <div>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-      </div>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-      <ReactionButtons post={post} />
-    </article>
+    <PostExcerpt key={post.id} post={post} />
   ));
 
   return (
